55a37e80181a047c7bb00e36be45886b6ebec947
pawntoqueen <gayemsuner@gmail.com>
2019-03-21 12:40:15 +0300

    game management class added

--- eclipse-workspace/PROJECT II/src/MAIN.java
+++ eclipse-workspace/PROJECT II/src/MAIN.java
@@ -1,30 +1,10 @@
-import java.util.Random;
 
 public class MAIN {
 
 	public static void main(String[] args) throws Exception {
-		Random rnd= new Random();
-		int round=1;
-		System.out.println("--------------round"+round+"--------------");
-		int target_number= rnd.nextInt(899)+100;
-		System.out.println("Target Number: "+ target_number);
-		int[] numbers = new int[7]; 
-		System.out.println();
-		System.out.print("Numbers: ");
-		for (int i = 0; i < 5; i++) {
-			numbers[i] = rnd.nextInt(8)+1;
-			System.out.print(numbers[i]+" ");
-		}
-		numbers[6] = (rnd.nextInt(4)+1)*25;
-		System.out.println(numbers[6]);
-		System.out.println();
-		System.out.print("Duration: ");
-		for (int i = 30; i >=0; i--) {
-			System.out.print(" "+i);
-			Thread.sleep(100);
-		}
-		System.out.println();
-		System.out.println("------------------------------------");		
+		Game game = new Game();
+		game.printGameScreen();
 	}
-
+	
+	
 }
