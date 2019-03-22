0671ed355cd43d0980efbbf288c96811b901d7c0
TaigaShinyouju <TaigaShinyouju@users.noreply.github.com>
2019-03-22 11:19:35 +0300

    Random düzeltme

--- eclipse-workspace/PROJECT II/src/Game.java
+++ eclipse-workspace/PROJECT II/src/Game.java
@@ -8,13 +8,13 @@ public class Game {
 		Random rnd= new Random();
 		int round=1;
 		System.out.println("--------------Round " + round+"--------------");
-		int target_number= rnd.nextInt(899)+100;
+		int target_number= rnd.nextInt(900)+100;
 		System.out.println("Target Number: "+ target_number);
 		int[] numbers = new int[7]; 
 		System.out.println();
 		System.out.print("Numbers: ");
 		for (int i = 0; i < 5; i++) {
-			numbers[i] = rnd.nextInt(9)+1;
+			numbers[i] = rnd.nextInt(9) + 1;
 			System.out.print(numbers[i]+" ");
 		}
 		numbers[6] = (rnd.nextInt(4)+1)*25;
