ad6efd77dc55263b57926f88767d7bd452a530fd
TaigaShinyouju <TaigaShinyouju@users.noreply.github.com>
2019-03-25 11:26:36 +0300

    postfix (step amount still bugged)

--- eclipse-workspace/PROJECT II/src/MAIN.java
+++ eclipse-workspace/PROJECT II/src/MAIN.java
@@ -3,7 +3,9 @@ public class MAIN {
 
 	public static void main(String[] args) throws Exception {
 		Game game = new Game();
-		game.printGameScreen();
+		//game.printGameScreen();
+		
+		System.out.println("Result: " + game.postfixEvaluation("1 6 9 * 4 / 75 9 - * +"));
 	}
 	
 	
