452f35a7f8138ff317807aebe55c758b003a7a20
ahmet <ahmet@AHMET>
2019-03-29 10:46:07 +0300

    last

--- eclipse-workspace/PROJECT II/src/infixToPostfix.java
+++ eclipse-workspace/PROJECT II/src/infixToPostfix.java
@@ -5,51 +5,49 @@ import java.util.Stack;
 class infixToPostfix {
 
 	Stack stack = new Stack();
-	ArrayList<String> operators;
+	Stack temp = new Stack();
+	String [] operators = {"+", "-", "*", "/", "(", ")" };
 	String postfix = "";
 
 	public infixToPostfix(String infix) {
-
 		
-		operators = new ArrayList<String>();
-
-		operators.add("+");
-		operators.add("-");
-		operators.add("*");
-		operators.add("/");
-		operators.add("(");
-		operators.add(")");
-
-		System.out.println();
 		while (infix.length() > 0) {
 
 			String operand;
 			String operator;
 			boolean operation;
+			boolean notOperator = true;
 
-			if (!operators.contains(infix.substring(0, 1))) {
-				while (!operators.contains(infix.substring(0, 1)) && !infix.isEmpty()) {
+			for (int i = 0; i < operators.length; i++) {
+				if(operators[i].equals(infix.substring(0, 1)))
+					notOperator = false;
+			}
+			
+			if (notOperator) {
+				while (notOperator && !infix.isEmpty()) {
 					operand = infix.substring(0, 1);
 					postfix = postfix + operand;
 					infix = infix.substring(1);
+					for (int i = 0; i < operators.length; i++) {
+						if(operators[i].equals(infix.substring(0, 1)))
+							notOperator = false;
+					}
 				}
-				postfix += " ";
-				System.out.println(postfix);
-			} else if (operators.get(4).equals(infix.substring(0, 1))) {
+				System.out.println(postfix + "\t\t\t"); printStack();
+			} else if (operators[4].equals(infix.substring(0, 1))) {
 				stack.push(infix.substring(0, 1));
-				printStack();
 				infix = infix.substring(1);
-			} else if (operators.get(5).equals(infix.substring(0, 1))) {
+			} else if (operators[5].equals(infix.substring(0, 1))) {
 				while (!stack.peek().equals("(")) {
 					postfix = postfix + " " + stack.pop();
-					System.out.println(postfix);
+					System.out.println(postfix + "\t\t\t"); printStack();
 				}
 				stack.pop();
 				infix = infix.substring(1);
 			} else {
 				operator = infix.substring(0, 1);
 
-				if (!stack.isEmpty()) { 
+				if (!stack.isEmpty()) {
 					do {
 						operation = true;
 						if ((operator.equals("*") || operator.equals("/"))
@@ -59,12 +57,13 @@ class infixToPostfix {
 							operation = false;
 						if (operation == true) {
 							postfix = postfix + " " + stack.pop();
-							System.out.println(postfix);
+							System.out.println(postfix + "\t\t\t"); printStack();
 						}
 
 					} while (operation && !stack.isEmpty());
 				}
 				stack.push(operator);
+				System.out.println(postfix + "\t\t\t"); printStack();
 				infix = infix.substring(1);
 				postfix += " ";
 			}
@@ -80,13 +79,20 @@ class infixToPostfix {
 	}
 	
 	public void printStack() {
-		Stack tempstack = new Stack();
-		while (! (stack.isEmpty())) {
-		System.out.println(stack.peek());
-		tempstack.push(stack.pop());
+		
+		System.out.println();
+		while(!stack.isEmpty()) {
+			
+			System.out.println("| " + stack.peek() + " |");
+			temp.push(stack.pop());
+			
 		}
-		while (! (stack.isEmpty()))
-		stack.push(tempstack.pop());
+		System.out.println("-----");
+		while(!temp.isEmpty()) {
+			stack.push(temp.pop());
+		}
+		System.out.println();
+		
 	}
 
 	
